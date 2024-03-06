3095e0bac5014bf6f57696f391019d03d0bdca89
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-03-06 16:32:43 +0330

    feat: added form structure

--- src/components/formSection/FormSection.tsx
+++ src/components/formSection/FormSection.tsx
@@ -21,25 +21,28 @@ const FormSection = (step : number) => {
             case 2:
                 return (
                     <div className={s.formSection}>
-                        <p className={s.title}>Personal Info</p>
+                        <p className={s.title}>Select your plan</p>
+                        <p className={s.description}>You have the option of monthly or annual billing.</p>
                     </div> 
                 )
             case 3:
                 return (
                     <div className={s.formSection}>
-                        <p className={s.title}>Personal Info</p>
+                        <p className={s.title}>Pick add-ons</p>
+                        <p className={s.description}>Addons help enhance the gaming experience.</p>
                     </div> 
                 )
             case 4:
                 return (
                     <div className={s.formSection}>
-                        <p className={s.title}>Personal Info</p>
+                        <p className={s.title}>Finishing up</p>
+                        <p className={s.description}>Double-check everything looks okay before confirming.</p>
                     </div> 
                 )
             case 5:
                 return (
                     <div className={s.formSection}>
-                        <p className={s.title}>Personal Info</p>
+                        <p className={s.title}>big thancc</p>
                     </div> 
                 )
         }
