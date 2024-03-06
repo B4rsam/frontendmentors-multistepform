a06b6bfb44b5ff3962484742fe43dfe82e28ea3b
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-03-06 14:52:47 +0330

    feat: basic styling

--- src/components/formSteps/FormSteps.tsx
+++ src/components/formSteps/FormSteps.tsx
@@ -8,10 +8,10 @@ interface formSteps {
 const FormSteps : FC<formSteps>= ({changeStep}) => {
     return (
         <div className={s.formSteps}>
-            <StepButton stepContent="content" step={1} stepLabel='Step 1' changeStep={changeStep}/>
-            <StepButton stepContent="content" step={2} stepLabel='Step 2' changeStep={changeStep}/>
-            <StepButton stepContent="content" step={3} stepLabel='Step 3' changeStep={changeStep}/>
-            <StepButton stepContent="content" step={4} stepLabel='Step 4' changeStep={changeStep}/>
+            <StepButton stepContent="YOUR INFO" step={1} stepLabel='STEP 1' changeStep={changeStep}/>
+            <StepButton stepContent="SELECT PLAN" step={2} stepLabel='STEP 2' changeStep={changeStep}/>
+            <StepButton stepContent="ADD-ONS" step={3} stepLabel='STEP 3' changeStep={changeStep}/>
+            <StepButton stepContent="SUMMARY" step={4} stepLabel='STEP 4' changeStep={changeStep}/>
         </div>
     )
 }
