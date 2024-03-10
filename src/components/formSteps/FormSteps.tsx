f4e7bb0b1cfc64206732c33f6c3691e3a617ba27
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-03-10 14:49:50 +0330

    feat: stepButton mobile styling

--- src/components/formSteps/FormSteps.tsx
+++ src/components/formSteps/FormSteps.tsx
@@ -9,10 +9,12 @@ interface formSteps {
 const FormSteps : FC<formSteps>= ({changeStep, step}) => {
     return (
         <div className={s.formSteps}>
-            <StepButton currentStep={step} stepContent="YOUR INFO" step={1} stepLabel='STEP 1' changeStep={changeStep}/>
-            <StepButton currentStep={step} stepContent="SELECT PLAN" step={2} stepLabel='STEP 2' changeStep={changeStep}/>
-            <StepButton currentStep={step} stepContent="ADD-ONS" step={3} stepLabel='STEP 3' changeStep={changeStep}/>
-            <StepButton currentStep={step} stepContent="SUMMARY" step={4} stepLabel='STEP 4' changeStep={changeStep}/>
+            <div className={s.stepContainer}>
+                <StepButton currentStep={step} stepContent="YOUR INFO" step={1} stepLabel='STEP 1' changeStep={changeStep}/>
+                <StepButton currentStep={step} stepContent="SELECT PLAN" step={2} stepLabel='STEP 2' changeStep={changeStep}/>
+                <StepButton currentStep={step} stepContent="ADD-ONS" step={3} stepLabel='STEP 3' changeStep={changeStep}/>
+                <StepButton currentStep={step} stepContent="SUMMARY" step={4} stepLabel='STEP 4' changeStep={changeStep}/>    
+            </div>
         </div>
     )
 }
