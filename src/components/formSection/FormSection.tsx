94e42927691d5cfc8b5900ddc67d6073b65d1449
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-03-13 15:09:42 +0330

    feat,fix: form summary modularized, fixed mobile interface

--- src/components/formSection/FormSection.tsx
+++ src/components/formSection/FormSection.tsx
@@ -4,11 +4,11 @@ import thanks from '../../assets/images/icon-thank-you.svg'
 import arcade from '../../assets/images/icon-arcade.svg'
 import advanced from '../../assets/images/icon-advanced.svg'
 import pro from '../../assets/images/icon-pro.svg'
-import Button from '../primitives/Button/Button'
 import FormInfo from '../formSectionSteps/formInfo/FormInfo'
 import FormPlan from '../formSectionSteps/formPlan/FormPlan'
 import FormAddon from '../formSectionSteps/formAddon/FormAddon'
 import FormFinal from '../formSectionSteps/formFinal/FormFinal'
+import FormSummary from '../formSectionSteps/formSummary/FormSummary'
 
 interface formSection {
     step: number
@@ -37,51 +37,7 @@ const FormSection : FC<formSection> = ({step, plan, total, services, handlePlan,
                 )
             case 4:
                 return (
-                    <div className={s.formSection}>
-                        <p className={s.title}>Finishing up</p>
-                        <p className={s.description}>Double-check everything looks okay before confirming.</p>
-                        <div className={s.services}>
-                            <div className={s.planSection}>
-                                { plan === 0 ? <div className={s.planDetails}>
-                                    <div className={s.options}>
-                                        <p className={s.planTitle}>Arcade</p>
-                                        <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
-                                    </div>
-                                    <p className={s.planPrice}>$9/mo</p>
-                                </div> : plan === 1 ? <div className={s.planDetails}> 
-                                    <div className={s.options}>
-                                            <p className={s.planTitle}>Advanced</p>
-                                            <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
-                                        </div>
-                                        <p className={s.planPrice}>$12/mo</p>
-                                </div> : plan === 2 ? <div className={s.planDetails}>
-                                    <div className={s.options}>
-                                            <p className={s.planTitle}>Pro</p>
-                                            <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
-                                        </div>
-                                        <p className={s.planPrice}>$15/mo</p>
-                                </div> : null}
-                            </div>
-                            <div className={s.addonSection}>
-                                {services.online ? <div className={`${s.addon} ${s.addonOnline}`}>
-                                    <p className={s.addonTitle}>Online service</p>
-                                    <p className={s.addonPrice}>+$1/mo</p>
-                                </div> : null}
-                                {services.storage ? <div className={`${s.addon} ${s.addonStorage}`}>
-                                    <p className={s.addonTitle}>Cloud Storage</p>
-                                    <p className={s.addonPrice}>+$2/mo</p>
-                                </div> : null}
-                                {services.profile ? <div className={`${s.addon} ${s.addonProfile}`}>
-                                    <p className={s.addonTitle}>Profile Customization</p>
-                                    <p className={s.addonPrice}>+$2/mo</p>
-                                </div> : null}
-                            </div>
-                        </div>
-                        <div className={s.pricing}>
-                            <p className={s.priceDetails}>Total (per month)</p>
-                            <p className={s.totalPrice}>{`+$${total}/mo`}</p>
-                        </div>
-                    </div> 
+                    <FormSummary services={services} plan={plan} total={total} changeStep={changeStep}/>
                 )
             case 5:
                 return (
---
+++ src/components/formSectionSteps/formSummary/FormSummary.tsx
@@ -0,0 +1,61 @@
+import s from './formsummary.module.sass'
+import Button from '../../primitives/Button/Button'
+import { FC } from 'react'
+
+interface IFormSummary {
+    plan: number
+    services: any
+    total: number
+    changeStep: (where: number) => void
+}
+const FormSummary : FC<IFormSummary>= ({plan, services, total, changeStep}) => {
+    return (
+        <div className={s.formSection}>
+            <p className={s.title}>Finishing up</p>
+            <p className={s.description}>Double-check everything looks okay before confirming.</p>
+            <div className={s.services}>
+                <div className={s.planSection}>
+                    { plan === 0 ? <div className={s.planDetails}>
+                        <div className={s.options}>
+                            <p className={s.planTitle}>Arcade</p>
+                            <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
+                        </div>
+                        <p className={s.planPrice}>$9/mo</p>
+                    </div> : plan === 1 ? <div className={s.planDetails}> 
+                        <div className={s.options}>
+                                <p className={s.planTitle}>Advanced</p>
+                                <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
+                            </div>
+                            <p className={s.planPrice}>$12/mo</p>
+                    </div> : plan === 2 ? <div className={s.planDetails}>
+                        <div className={s.options}>
+                                <p className={s.planTitle}>Pro</p>
+                                <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
+                            </div>
+                            <p className={s.planPrice}>$15/mo</p>
+                    </div> : null}
+                </div>
+                <div className={s.addonSection}>
+                    {services.online ? <div className={`${s.addon} ${s.addonOnline}`}>
+                        <p className={s.addonTitle}>Online service</p>
+                        <p className={s.addonPrice}>+$1/mo</p>
+                    </div> : null}
+                    {services.storage ? <div className={`${s.addon} ${s.addonStorage}`}>
+                        <p className={s.addonTitle}>Cloud Storage</p>
+                        <p className={s.addonPrice}>+$2/mo</p>
+                    </div> : null}
+                    {services.profile ? <div className={`${s.addon} ${s.addonProfile}`}>
+                        <p className={s.addonTitle}>Profile Customization</p>
+                        <p className={s.addonPrice}>+$2/mo</p>
+                    </div> : null}
+                </div>
+            </div>
+            <div className={s.pricing}>
+                <p className={s.priceDetails}>Total (per month)</p>
+                <p className={s.totalPrice}>{`+$${total}/mo`}</p>
+            </div>
+        </div> 
+    )
+}
+
+export default FormSummary
