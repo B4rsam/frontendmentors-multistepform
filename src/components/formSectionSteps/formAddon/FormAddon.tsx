b6fc48786fc36ff40949222da7ef38ed89f34f8d
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-03-16 11:55:00 +0330

    feat: formAddon annual billing

--- src/components/formSectionSteps/formAddon/FormAddon.tsx
+++ src/components/formSectionSteps/formAddon/FormAddon.tsx
@@ -12,9 +12,9 @@ const FormAddon : FC<IFormAddon>= ({data, handleService }) => {
         <div className={s.formSection}>
             <p className={s.title}>Pick add-ons</p>
             <p className={s.description}>addons help enhance the gaming experience.</p>
-            <Checkbox defaultChecked={data.online} title="Online service" description='Access to online multiplayer games' price="+$1/mo" id={0} handleService={(e : any) => handleService(e.target.checked,0)}/>
-            <Checkbox defaultChecked={data.storage} title="Larger storage" description='Extra 1TB cloud storage space' price="+$2/mo" id= {1} handleService={(e : any) => handleService(e.target.checked,1)}/>
-            <Checkbox defaultChecked={data.profile} title="Customizable profile" description='Custom profile theme' price="+$2/mo" id={2} handleService={(e : any) => handleService(e.target.checked,2)}/>
+            <Checkbox defaultChecked={data.online} title="Online service" description='Access to online multiplayer games' price={data.annual ? "+$10/yr" : "+$1/mo"} id={0} handleService={(e : any) => handleService(e.target.checked,0)}/>
+            <Checkbox defaultChecked={data.storage} title="Larger storage" description='Extra 1TB cloud storage space' price={data.annual ? "+$20/yr" : "+$2/mo"} id= {1} handleService={(e : any) => handleService(e.target.checked,1)}/>
+            <Checkbox defaultChecked={data.profile} title="Customizable profile" description='Custom profile theme' price={data.annual ? "+$20/yr" : "+$2/mo"} id={2} handleService={(e : any) => handleService(e.target.checked,2)}/>
         </div> 
     )
 }
