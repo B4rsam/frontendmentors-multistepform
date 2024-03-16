6654176fc33d65fbd89a8052675525a9708c040d
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-03-16 12:01:04 +0330

    feat: formSummary annual billing

--- src/components/formSectionSteps/formSummary/FormSummary.tsx
+++ src/components/formSectionSteps/formSummary/FormSummary.tsx
@@ -17,42 +17,42 @@ const FormSummary : FC<IFormSummary>= ({plan, data, total, changeStep}) => {
                 <div className={s.planSection}>
                     { plan === 0 ? <div className={s.planDetails}>
                         <div className={s.options}>
-                            <p className={s.planTitle}>Arcade</p>
+                            <p className={s.planTitle}>Arcade{data.annual ? " (Annual)" : null}</p>
                             <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
                         </div>
-                        <p className={s.planPrice}>$9/mo</p>
+                        <p className={s.planPrice}>{data.annual ? "$90/yr" : "$9/mo"}</p>
                     </div> : plan === 1 ? <div className={s.planDetails}> 
                         <div className={s.options}>
-                                <p className={s.planTitle}>Advanced</p>
+                                <p className={s.planTitle}>Advanced{data.annual ? " (Annual)" : null}</p>
                                 <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
                             </div>
-                            <p className={s.planPrice}>$12/mo</p>
+                            <p className={s.planPrice}>{data.annual ? "$120/yr" : "$12/mo"}</p>
                     </div> : plan === 2 ? <div className={s.planDetails}>
                         <div className={s.options}>
-                                <p className={s.planTitle}>Pro</p>
+                                <p className={s.planTitle}>Pro{data.annual ? " (Annual)" : null}</p>
                                 <Button type="summaryBack" children="Change" className={s.planChange} onClick={() => changeStep(2)}/>
                             </div>
-                            <p className={s.planPrice}>$15/mo</p>
+                            <p className={s.planPrice}>{data.annual ? "$150/yr" : "$15/mo"}</p>
                     </div> : null}
                 </div>
                 <div className={s.addonSection}>
                     {data.online ? <div className={`${s.addon} ${s.addonOnline}`}>
                         <p className={s.addonTitle}>Online service</p>
-                        <p className={s.addonPrice}>+$1/mo</p>
+                        <p className={s.addonPrice}>{data.annual ? "+$10/yr" : "+$1/mo"}</p>
                     </div> : null}
                     {data.storage ? <div className={`${s.addon} ${s.addonStorage}`}>
                         <p className={s.addonTitle}>Cloud Storage</p>
-                        <p className={s.addonPrice}>+$2/mo</p>
+                        <p className={s.addonPrice}>{data.annual ? "+$20/yr" : "+$2/mo"}</p>
                     </div> : null}
                     {data.profile ? <div className={`${s.addon} ${s.addonProfile}`}>
                         <p className={s.addonTitle}>Profile Customization</p>
-                        <p className={s.addonPrice}>+$2/mo</p>
+                        <p className={s.addonPrice}>{data.annual ? "+$20/yr" : "+$2/mo"}</p>
                     </div> : null}
                 </div>
             </div>
             <div className={s.pricing}>
-                <p className={s.priceDetails}>Total (per month)</p>
-                <p className={s.totalPrice}>{`+$${total}/mo`}</p>
+                <p className={s.priceDetails}>Total{data.annual ? " (per year)" : " (per month)"}</p>
+                <p className={s.totalPrice}>{data.annual ? `+$${total}/yr` : `+$${total}/mo`}</p>
             </div>
         </div> 
     )
